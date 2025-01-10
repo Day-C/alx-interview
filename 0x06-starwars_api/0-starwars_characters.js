d9cd8904ecb9c024732bd194b542dfa5fe01eb6e
DayC <kongranalanyuy@gmail.com>
2025-01-10 10:26:10 +0000

    starwars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,32 +2,16 @@
 
 const request = require('request');
 
-// get the movie number from collamd line
-let season = process.argv[2];
-// conter negative inputs to posivite
-if (season < 0) {
-  season = season * -1;
-}
-
-// get the actors details
-const url = 'https://swapi-api.alx-tools.com/api/films/' + season;
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    // convert request body into  json format
-    const data = JSON.parse(body);
-    // console.log(data.title)
-    const actors = data.characters;
-
-    for (let i = 0; i < actors.length; i++) {
-      // console.log(actors[i])
-      // make a request to get actors name
-      request(actors[i], (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const actorsInfo = JSON.parse(body);
-          console.log(actorsInfo.name);
-        }
-      });
-    }
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
-// get characters in the season from the above response
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
